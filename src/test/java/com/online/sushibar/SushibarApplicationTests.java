3984ad02032077613b49acb6e2ed850038bbe44a
Beksultan Tatenov <bekatatenov@gmail.com>
2021-03-09 17:48:20 +0600

    Initial commit

---
+++ src/test/java/com/online/sushibar/SushibarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.sushibar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushibarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
