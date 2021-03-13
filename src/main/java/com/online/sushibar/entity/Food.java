847d00449675e3850cfb4cb4452013add79fcf49
Beksultan Tatenov <bekatatenov@gmail.com>
2021-03-13 14:56:03 +0600

    add Volidation to Model Food

--- src/main/java/com/online/sushibar/entity/Food.java
+++ src/main/java/com/online/sushibar/entity/Food.java
@@ -5,6 +5,10 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,12 +19,22 @@ public class Food {
     @Id
     private Long id;
 
+    @NotBlank
+    @Size(min = 1, max = 128)
     private String name;
+
+    @NotBlank
+    @Size(min = 1, max = 128)
     private String image;
 
+    @Positive
     private Double gram;
+    @Positive
     private Double quantity;
+    @Positive
     private Double price;
+    @NotBlank
+    @Size(min = 10, max = 240)
     private String description;
 
     @ManyToOne
