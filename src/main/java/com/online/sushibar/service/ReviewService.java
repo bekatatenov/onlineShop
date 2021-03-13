00bad066b1ac09920afcbba7c09631b372dd7316
Beksultan Tatenov <bekatatenov@gmail.com>
2021-03-13 15:41:16 +0600

    add Service for User And Review

---
+++ src/main/java/com/online/sushibar/service/ReviewService.java
@@ -0,0 +1,12 @@
+package com.online.sushibar.service;
+
+import com.online.sushibar.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+    private final ReviewRepository reviewRepository;
+
+}
---
+++ src/main/java/com/online/sushibar/service/UserService.java
@@ -0,0 +1,12 @@
+package com.online.sushibar.service;
+
+import com.online.sushibar.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+}
