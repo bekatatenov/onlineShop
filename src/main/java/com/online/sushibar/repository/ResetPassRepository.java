6b7f98eb12775077f28fa38132d94977ccb73b9f
Beksultan Tatenov <bekatatenov@gmail.com>
2021-03-29 14:30:17 +0600

    add repository for reset pass

---
+++ src/main/java/com/online/sushibar/repository/ResetPassRepository.java
@@ -0,0 +1,7 @@
+package com.online.sushibar.repository;
+
+import com.online.sushibar.entity.ResetPass;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResetPassRepository extends JpaRepository<ResetPass, Long> {
+}
